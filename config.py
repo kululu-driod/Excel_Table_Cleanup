c16fa680cd6f6533f78a0146a7fb1379e7271367
Yvonne-Ng <ywng@uci.edu>
2020-07-09 09:50:10 +0800

    working remove header, with config

---
+++ config.py
@@ -0,0 +1,14 @@
+
+# ---Specify here header/footer keyword to be removed, using the  --keep_header flag removes the effect
+header_keyword=["K C Tang Consultants Ltd.", "OFFICE RENOVATION FOR HKSAPID AT", "OFFICE RENOVATION FOR HKSAPID AT", "SCHEDULE OF WORKS", "SCHEDULE NO. 1 - PRELIMINARIES", "LEK YUEN COMMUNITY HALL, SHATIN, NT", "UNIT NO. 2+7, G/F", "SCHEDULE NO.", "LEK YUEN COMMUNITY HALL,SHAUN, NT"]
+
+
+# ---Specify here header/footer keyword where rows that contains them will be removed, using the the --keep_header flag ignores header removoal
+header_contain=["HKSAPID_SOW", "HKSAPIDSOW", "SCHEDULE NO."]
+
+#----REMOVE the following words
+#remove
+
+# ---Under Construction
+# ---spliting lines by capitalizaition
+cap_is_newline=False
--- direct_conversion.py
+++ direct_conversion.py
@@ -1,8 +1,8 @@
 import csv
-import numpy as np
 import re
 
 import argparse
+from config import *
 
 """ This code splits the lines from an csv input file"""
 """ Remove designated header/footers """
@@ -27,24 +27,6 @@ parser.add_argument('--table_split', dest='table_split',default=False,
                     help="Auto figure out and splitting tables")
 args = parser.parse_args()
 
-# specify here header/footer keyword
-header_keyword=["K C Tang Consultants Ltd.", "OFFICE RENOVATION FOR HKSAPID AT", "OFFICE RENOVATION FOR HKSAPID AT", "SCHEDULE OF WORKS", "SCHEDULE NO. 1 - PRELIMINARIES", "LEK YUEN COMMUNITY HALL, SHATIN, NT", "UNIT NO. 2+7, G/F", "SCHEDULE NO.", "LEK YUEN COMMUNITY HALL,SHAUN, NT"]
-
-
-header_contain=["HKSAPID_SOW", "HKSAPIDSOW"]
-
-
-# ---Under Construction
-# ---spliting lines by capitalizaition
-cap_is_newline=False
-
-
-def transpose_2d_list(l_2d):
-
-    arr_t = np.array(l_2d).T
-    l_2d_t = np.array(l_2d).T.tolist()
-
-    return l_2d_t
 
 def transpose_2d_list_string(l_2d):
     max_length=max([len(item ) for item in l_2d])
@@ -84,26 +66,25 @@ def clean_2dlist(l_2d, ugly_chars=["  "]):
 
 def remove_header(l_2d, header_keyword=header_keyword, header_contain=header_contain):
     output=[]
-    print("input_l_2d", l_2d)
     for i_row, row in enumerate(l_2d):
         skip_row=False
         for item in row:
 
-            print("item: ", item)
+            #print("item: ", item)
             for keyword in header_keyword:
-                print("keyword", keyword)
+                #print("keyword", keyword)
                 if keyword==item:
-                    print("keyword matched: ", keyword)
+                    #print("keyword matched: ", keyword)
                     skip_row=True
             for contain_keyword in header_contain:
-                print(contain_keyword)
+                #print(contain_keyword)
                 if contain_keyword in item:
-                    print("keyword contained: ", contain_keyword)
+                    #print("keyword contained: ", contain_keyword)
                     skip_row=True
         if skip_row==False:
-            print("row appending :", row)
+            #print("row appending :", row)
             output.append(row)
-        print("output", output)
+        #print("output", output)
     return output
 
 
@@ -117,10 +98,14 @@ with open("test.csv", newline='') as csvfile:
 
     spamwriter = csv.writer(output_csv,delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+    # --smart transpose
+    # --clean
+    # --remove header
+    # --split table
+
     #TODO call the following smart split line
     for i_row, row in enumerate(spamreader):
         # ---Getting the items in the rows
-
 
         l_2d=[]
         # --- For items
@@ -146,12 +131,12 @@ with open("test.csv", newline='') as csvfile:
         else:
             l_2d_trans=transpose_2d_list_string(l_2d_cleaned)
 
-        print("l_2d_trans:", l_2d_trans)
+        #print("l_2d_trans:", l_2d_trans)
         # remove header
         #if not args.keep_header:
         l_2d_trans=remove_header(l_2d_trans)
 
-        print("l_2d_trans after:", l_2d_trans)
+        #print("l_2d_trans after:", l_2d_trans)
 
 
 
